bb9b6a45120c0281e213ae49d940f4e46c3298aa
kleinzd <kleinzmail@gmail.com>
2023-06-22 21:55:44 +0800

    a

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -9,7 +9,7 @@
         <h1>Hi! I'm Kleinz!</h1>
         <p>15 year old high school student & web developer</p>
         <div class="d-flex flex-row gap-2">
-          <a class="btn btn-info" href="https://github.com/kleinzd"><i class="bi bi-github"></i> Github</a>
+          <a class="btn btn-info" href="https://github.com/kleinzv"><i class="bi bi-github"></i> Github</a>
           <a class="btn btn-success" disabled>Works</a>
         </div>
       </div>
